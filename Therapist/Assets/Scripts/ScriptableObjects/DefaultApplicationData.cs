e225c3da0b3b52801ced23073b63ac5844576ee1
BonoTheBonkers <kbonecki@us.edu.pl>
2018-03-14 22:08:31 +0100

    Cleanup

--- Therapist/Assets/Scripts/ScriptableObjects/DefaultApplicationData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DefaultApplicationData", menuName = "Therapist/Singletons/DefaultApplicationData", order = 1)]
-public class DefaultApplicationData : SingletonScriptableObject<DefaultApplicationData>
-{
-    public FApplicationData defaultApplicationData = new FApplicationData();
-}
